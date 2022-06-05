46d29e0d5a3e3604699d0c82e1a8ed347febedae
aman0207 <aman27verma.mohit@gmail.com>
2022-06-05 11:47:42 +0530

    initial box added

---
+++ src/components/Box.jsx
@@ -0,0 +1,15 @@
+
+export default function Box() {
+    return <mesh 
+    rotation={[
+        90, 0, 20
+    ]}>
+        <boxBufferGeometry
+        attach="geometry"
+        args={[3, 3, 3]} />
+        <meshLambertMaterial 
+        attach="material"
+        color="blue"
+        />
+    </mesh>
+} 
